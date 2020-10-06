42b9ca959a4163e130e2ff999e21a6c635a76ac3
Oleh Kilaru <kilaruoleh@gmail.com>
2020-10-06 16:06:20 +0300

    updated MergeSort

--- src/main/java/co/inventorsoft/homework/MergeSort.java
+++ src/main/java/co/inventorsoft/homework/MergeSort.java
@@ -6,52 +6,37 @@ import java.util.Scanner;
 public class MergeSort {
     public static void main(String[] args) {
         int[] arr = getNumbers();
-        arr = mergeSort(arr);
+        mergeSort(arr);
         System.out.println(Arrays.toString(arr));
     }
 
-    public static int[] mergeSort(int[] array) {
-        int[] tmp;
-        int[] currentSrc = array;
-        int[] currentDest = new int[array.length];
-
-        int size = 1;
-        while (size < array.length) {
-            for (int i = 0; i < array.length; i += 2 * size) {
-                merge(currentSrc, i, currentSrc, i + size, currentDest, i, size);
-            }
-
-            tmp = currentSrc;
-            currentSrc = currentDest;
-            currentDest = tmp;
-
-            size = size * 2;
-
-            System.out.println(Arrays.toString(currentSrc));
-        }
-        return currentSrc;
+    public static void mergeSort(int[] input) {
+        mergesort(input, 0, input.length - 1);
     }
 
-    private static void merge(int[] src1, int src1Start, int[] src2, int src2Start, int[] dest,
-                              int destStart, int size) {
-        int index1 = src1Start;
-        int index2 = src2Start;
-
-        int src1End = Math.min(src1Start + size, src1.length);
-        int src2End = Math.min(src2Start + size, src2.length);
-
-        int iterationCount = src1End - src1Start + src2End - src2Start;
-
-        for (int i = destStart; i < destStart + iterationCount; i++) {
-            if (index1 < src1End && (index2 >= src2End || src1[index1] < src2[index2])) {
-                dest[i] = src1[index1];
-                index1++;
-            } else {
-                dest[i] = src2[index2];
-                index2++;
-            }
+    private static void mergesort(int[] input, int start, int end) {
+        int mid = (start + end) / 2;
+        if (start < end) {
+            mergesort(input, start, mid);
+            mergesort(input, mid + 1, end);
         }
 
+        int i = 0, first = start, last = mid + 1;
+        int[] tmp = new int[end - start + 1];
+
+        while (first <= mid && last <= end) {
+            tmp[i++] = input[first] < input[last] ? input[first++] : input[last++];
+        }
+        while (first <= mid) {
+            tmp[i++] = input[first++];
+        }
+        while (last <= end) {
+            tmp[i++] = input[last++];
+        }
+        i = 0;
+        while (start <= end) {
+            input[start++] = tmp[i++];
+        }
     }
 
     public static int[] getNumbers() {
